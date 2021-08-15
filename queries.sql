2eda6e3d6350b7878df88a3332cdce03a7193318
YuriyMarks <ymarkovka@gmail.com>
2021-08-15 14:50:02 +0300

    При получении списока постов с сортировкой по популярности использовано не количество просмотров, а количество лайков

--- queries.sql
+++ queries.sql
@@ -4,25 +4,23 @@ USE readme;
 -- Записываем данные (список типов контента для поста)
 INSERT INTO content_types
 VALUES
-	(DEFAULT, 'Текст', 'text'),
-	(DEFAULT, 'Цитата', 'quote'),
-	(DEFAULT, 'Картинка', 'photo'),
-	(DEFAULT, 'Видео', 'video'),
-	(DEFAULT, 'Ссылка', 'link');
+	('Текст', 'text'),
+	('Цитата', 'quote'),
+	('Картинка', 'photo'),
+	('Видео', 'video'),
+	('Ссылка', 'link');
 
 -- Добавляем пользователей
 INSERT INTO users
 VALUES
-	(DEFAULT, NOW(), 'larisa@gmail.com', 'Лариса', "qwerty", ''),
-	(DEFAULT, NOW(), 'vladik@gmail.com', 'Владик', "12345", ''),
-	(DEFAULT, NOW(), 'viktor@gmail.com', 'Виктор', "qaz", '');
+	('larisa@gmail.com', 'Лариса', 'qwerty'),
+	('vladik@gmail.com', 'Владик', '12345'),
+	('viktor@gmail.com', 'Виктор', 'qaz');
 
 -- Добавляем существующий список постов
 INSERT INTO posts
 VALUES
-	(
-		DEFAULT, 
-		NOW(), 
+	( 
 		'Цитата', 
 		'Мы в жизни любим только раз, а после ищем лишь похожих', 
 		'Лариса', 
@@ -34,8 +32,6 @@ VALUES
 		2
 	),
 	(
-		DEFAULT, 
-		NOW(), 
 		'Игра престолов', 
 		'Не могу дождаться начала финального сезона своего любимого сериала!', 
 		'Владик', 
@@ -47,8 +43,6 @@ VALUES
 		2
 	),
 	(
-		DEFAULT,
-		NOW(),
 		'Наконец, обработал фотки!',
 		'',
 		'Виктор',
@@ -60,8 +54,6 @@ VALUES
 		3
 	),
 	(
-		DEFAULT,
-		NOW(),
 		'Моя мечта',
 		'',
 		'Лариса',
@@ -74,8 +66,6 @@ VALUES
 
 	),
 	(
-		DEFAULT,
-		NOW(),
 		'Лучшие курсы',
 		'',
 		'Владик',
@@ -90,21 +80,26 @@ VALUES
 -- Добавляем три комментария
 INSERT INTO comments
 VALUES
-	(DEFAULT, NOW(), 'Отличный сериал!', 1, 2),
-	(DEFAULT, NOW(), 'Сериал бомба!', 3, 2),
-	(DEFAULT, NOW(), 'Шикарная цитата!', 2, 1);
+	('Отличный сериал!', 1, 2),
+	('Сериал бомба!', 3, 2),
+	('Шикарная цитата!', 2, 1);
 
 -- Запросы для этих действий
 -- Получить список постов с сортировкой по популярности и вместе с именами авторов и типом контента
-SELECT content, num_of_views, login, type FROM posts
-	INNER JOIN users ON users.id = posts.user_id
-	INNER JOIN content_types ON content_types.id = posts.content_type_id
-	ORDER BY num_of_views DESC;
+SELECT results.post_id, content, login, content_types.type, results.likes_num FROM (
+	SELECT COUNT(post_id) as likes_num, post_id FROM likes
+	GROUP BY post_id
+) AS results
+	RIGHT JOIN posts ON results.post_id = posts.user_id
+	LEFT JOIN users ON users.id = posts.user_id
+	LEFT JOIN content_types ON content_types.id = posts.content_type_id
+	GROUP BY post_id
+	ORDER BY likes_num DESC;
 
 -- Получить список постов для конкретного пользователя
 SELECT content, login FROM posts
 	INNER JOIN users ON users.id = posts.user_id
-	WHERE login = 'Владик';
+	WHERE users.id = 2;
 
 -- Получить список комментариев для одного поста, в комментариях должен быть логин пользователя
 SELECT comment, login FROM comments
@@ -112,7 +107,12 @@ SELECT comment, login FROM comments
 	WHERE post_id = 2;
 
 -- Добавить лайк к посту
-INSERT INTO likes VALUES(1, 2);
+INSERT INTO likes VALUES
+					(1, 2),
+					(1, 2),
+					(1, 2),
+					(2, 3),
+					(3, 2);
 
 -- Подписаться на пользователя
 INSERT INTO subscriptions VALUES (1, 2);
