221a4fc284b811492f1aeef47c722fe933ae81a3
YuriyMarks <ymarkovka@gmail.com>
2021-09-06 21:27:57 +0300

    Добавлен код валидации формы и показа ошибок

---
+++ registration.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'helpers.php';
+require_once 'functions.php';
+
+$filePath = '';
+$keys = [
+	'email' => 'ЭЛЕКТРОННАЯ ПОЧТА',
+	'login' => 'ЛОГИН',
+	'password' => 'ПАРОЛЬ',
+	'password-repeat' => 'ПОВТОР ПАРОЛЯ',
+];
+$rules = [
+	'email' => function() {
+		return validateEmail('email', $_POST);
+	},
+	'login' => function() {},
+	'password' => function() {},
+	'password-repeat' => function() {},
+];
+$required = ['email', 'login', 'password', 'password-repeat'];
+$errors = [];
+$modErrors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$errors = validateRegForm($rules, $required);
+	$con = mysqliConnect();
+
+	if (!isset($errors['email'])) {
+		$errors['email'] = checkEmail($con, $_POST['email']);
+	}
+
+	if ($_POST['password'] !== $_POST['password-repeat']) {
+		$errors['password'] = 'Пароли не совпадают';
+	}
+
+	if ($_FILES['userpic-file']['name']) {
+		if($type = mime_content_type($_FILES['userpic-file']['tmp_name'])) {
+			$ext = explode('/', $type)[1];
+            $fileName = uniqid() . '.' . $ext;
+            $path = __DIR__ . '/uploads/' . $fileName;
+            $filePath = $fileName;
+
+			moveLoadedFile($_FILES['userpic-file']['tmp_name'], $path);
+		} else {
+			$errors['file'] = $error;
+		}
+	}
+
+	$errors = array_filter($errors);
+	$modErrors = modifyErrors($errors, $keys);
+
+	if (!$errors) {
+		$passwordHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		insertUsersDataToDatabase($con, $filePath, $passwordHash, $_POST);
+	}
+}
+
+$mainContent = include_template('registration-main.php', ['errors' => $errors, 'modErrors' => $modErrors]);
+$layout = include_template('registration-layout.php', ['mainContent' => $mainContent]);
+print($layout);
